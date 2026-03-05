200a8127ce4d2b36024a444f9cbc76ac8e6e384c
hasanarofid <hasanarofid@gmail.com>
2026-03-05 21:30:55 +0700

    update client

--- aplikasi-presensi/src/api/client.js
+++ aplikasi-presensi/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://10.0.2.2:8000/api'; // Standard Android Emulator IP for localhost
+const API_URL = 'http://192.168.110.161:8000/api'; // Machine's local IP for physical device access
 
 const apiClient = axios.create({
   baseURL: API_URL,
